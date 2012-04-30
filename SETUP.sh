40955f4978d7a00c6307414356e1f0d287325320
Brandon Rhodes <brandon@rhodesmill.org>
2012-04-30 18:41:51 -0400

    Added the SETUP.sh script and the packages needed to build Python's virtualenv.

---
+++ SETUP.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# ~/.emacs.d/SETUP.sh script
+
+# Exit immediately if a command fails.
+set -e
+
+# Change directory to the directory containing this script.
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+# Create empty local.el if none exists.
+if [ ! -f local.el ]; then touch local.el ;fi
+
+# Unpack and install everything inside virtualenv named "~/.emacs/usr".
+USR=$PWD/usr
+
+DIRS=(Pymacs-0.23)
+rm -rf $USR $DIRS
+
+src/virtualenv.py $USR
+source $USR/src/bin/activate
+
+pip install src/closure_linter-2.3.5.tar.gz
+pip install src/pyflakes-0.5.0.tar.gz
+pip install src/rope-0.9.3.tar.gz
+pip install src/ropemacs-0.6.tar.gz
+
+# Copy pymacs.el to ~/.emacs.d
+tar xvfz src/Pymacs.tar.gz
+(cd Pymacs-0.23; python setup.py install)
+rm -f ./site-lisp/pymacs.el
+cp Pymacs-0.23/pymacs.el ./site-lisp/pymacs.el
+rm -rf $PWD/Pymacs-0.23
+
+# Byte-compile plain Emacs LISP files.
+emacs --batch -f batch-byte-compile ~/.emacs.d/site-lisp/*.el
