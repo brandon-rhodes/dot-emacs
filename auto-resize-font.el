4079138ab89a67fe9a14abc3ae00906f7d05e705
Brandon Rhodes <brandon@rhodesmill.org>
2012-05-03 09:51:09 -0400

    Bugfix: since several frames can exist at a time, a single previous-frame-width variable is not sufficient; a hash that can remember many frame sizes is necessary instead.

--- auto-resize-font.el
+++ auto-resize-font.el
@@ -1,18 +1,31 @@
 ;; Maintain at least 80 columns, even when the Emacs window is resized
 ;; Brandon Rhodes https://github.com/brandon-rhodes/dot-emacs
 
+;; The largest and smallest acceptible font sizes, in tenths of points.
+
 (setq maximum-font-height 180)
 (setq minimum-font-height 60)
-(setq previous-frame-width 0)
+
+;; The window change function gets called not only for frame resizing
+;; but during more minor events as well, so we remember the previous
+;; width of each frame so that we only kick into action when the frame
+;; is actually resized.
+
+(setq current-frame-widths (make-hash-table :test 'eq))
+
+;; Our window size change function itself, which hunts through font
+;; sizes in large-to-small order to find one that achieves at least 80
+;; columns.
 
 (setq window-size-change-functions
       '((lambda (frame)
-          (if (/= previous-frame-width (frame-width frame))
-              (let ((trial-size maximum-font-height))
-                (set-face-attribute 'default frame :height trial-size)
-                (while (and (> trial-size minimum-font-height)
-                            (< (frame-width frame) 80))
-                  (setq trial-size (- trial-size 10))
-                  (set-face-attribute 'default frame :height trial-size))
-                (setq previous-frame-width (frame-width frame))
-                )))))
+          (let ((trial-size maximum-font-height)
+                (previous-size (gethash frame current-frame-widths -1)))
+            (when (/= previous-size (frame-width frame))
+              (puthash frame (frame-width frame) current-frame-widths)
+              (set-face-attribute 'default frame :height trial-size)
+              (while (and (> trial-size minimum-font-height)
+                          (< (frame-width frame) 80))
+                (setq trial-size (- trial-size 10))
+                (set-face-attribute 'default frame :height trial-size))
+              )))))
