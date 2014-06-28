18b7214a005a1c07c55c30b7f6f214e7ea0f9eb0
Brandon Rhodes <brandon@rhodesmill.org>
2014-06-28 14:49:52 -0400

    Emacs has adjusted its comment indentation?

--- init.el
+++ init.el
@@ -356,10 +356,10 @@
 ;; Variables set through M-x customize-apropos.
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
  '(column-number-mode t)
  '(display-time-mode t)
@@ -389,8 +389,8 @@
  '(tooltip-mode nil)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(quote (mode-line-highlight ((t nil)))))
