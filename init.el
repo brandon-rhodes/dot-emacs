8ceef4951a720bdfaad7aa1a880ff74658bf256a
Brandon Rhodes <brhodes@dropbox.com>
2015-03-18 09:16:13 -0400

    Not sure why this "highlight" line keeps appearing

--- init.el
+++ init.el
@@ -443,4 +443,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight ((((class color) (min-colors 88)) :background "#eee8d5") (t :background "white")))
  '(quote (mode-line-highlight ((t nil)))))
