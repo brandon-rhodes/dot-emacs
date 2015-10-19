74514b859373474d6935f07ce1ca6654dd3cbf8d
Brandon Rhodes <brandon@rhodesmill.org>
2015-10-19 12:45:21 -0400

    Call magit-blame correctly

--- init.el
+++ init.el
@@ -288,7 +288,7 @@
 
 (require 'magit)
 (global-set-key (kbd "M-s") 'magit-status)
-(global-set-key (kbd "C-x v g") 'magit-blame-mode)
+(global-set-key (kbd "C-x v g") 'magit-blame)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (add-hook 'magit-mode-hook 'turn-on-visual-line-mode) ; wrap long commit msgs
 
