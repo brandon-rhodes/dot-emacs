a9a2e99ffe8f94331df182060a74dede4ca24e90
Brandon Rhodes <brandon@rhodesmill.org>
2016-03-07 09:22:50 -0500

    Add back a keyboard shortcut for Magit

--- init.el
+++ init.el
@@ -287,7 +287,7 @@
 ;; Magit
 
 (require 'magit)
-;(global-set-key (kbd "M-s") 'magit-status)
+(global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x v g") 'magit-blame)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (add-hook 'magit-mode-hook 'turn-on-visual-line-mode) ; wrap long commit msgs
