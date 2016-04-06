61161ee92159f69527ebb37f3d764ac452b9e2d6
Brandon Rhodes <brhodes@dropbox.com>
2016-04-06 16:42:23 -0400

    Make M-C-a "ag" search work even in js-mode

--- init.el
+++ init.el
@@ -107,6 +107,9 @@
 (eval-after-load 'coffee-mode
   '(define-key coffee-mode-map (kbd "M-C-a") 'ag-project))
 
+(eval-after-load 'js-mode
+  '(define-key js-mode-map (kbd "M-C-a") 'ag-project))
+
 ;; Never auto-split a frame into a left and right window.
 
 (setq split-width-threshold nil)
