c736c90925cd344f7b629fa0419aca8538e065c4
Brandon Rhodes <brhodes@dropbox.com>
2017-05-11 15:25:06 -0400

    Add width limit to inline "ag" search results
    This will prevent huge wide lines in files (common in minified js) from
    spamming the buffer where Emacs displays search results and making it
    nearly impossible to scroll past them.

--- init.el
+++ init.el
@@ -524,6 +524,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-arguments
+   (quote
+    ("--line-number" "--smart-case" "--nogroup" "--column" "--stats" "--width" "240" "--")))
  '(auto-save-default nil)
  '(coffee-tab-width 2)
  '(column-number-mode t)
