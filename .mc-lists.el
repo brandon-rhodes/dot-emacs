fa2ca445cd99b2817001a0af55d81772626caf7f
Brandon Rhodes <brhodes@dropbox.com>
2017-11-15 16:18:29 -0500

    Approve more multicursor commands

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,6 +3,12 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        c-electric-brace
+        c-electric-delete-forward
+        c-electric-lt-gt
+        c-electric-paren
+        c-electric-semi&comma
+        c-indent-line-or-region
         coffee-dedent-line-backspace
         comment-dwim
         delete-horizontal-space
