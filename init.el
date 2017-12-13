5d4f7b38d9ce8fd9a1c00ad3c82359128a7ddf41
Brandon Rhodes <brhodes@dropbox.com>
2017-12-13 12:48:35 -0500

    Also recognize WORKSPACE files as Bazel

--- init.el
+++ init.el
@@ -314,6 +314,7 @@
   )
 
 (add-to-list 'auto-mode-alist '("BUILD\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("WORKSPACE\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\.bzl\\'" . bzl-mode))
 (add-to-list 'auto-mode-alist '("\\.pyst\\'" . python-mode))
 
