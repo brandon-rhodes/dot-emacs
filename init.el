4dd5ff2165d54279a7178c826866496336bed898
Brandon Rhodes <brhodes@dropbox.com>
2018-03-01 10:10:27 -0500

    Have “ag” search hidden files and directories
    Some source code I dealt with today was inside a hidden directory, and
    “ag” was refusing to show me any of the functions or classes I was
    searching for.

--- init.el
+++ init.el
@@ -597,7 +597,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ag-arguments (quote ("--smart-case" "--stats" "--width" "240")))
+ '(ag-arguments (quote ("--hidden" "--smart-case" "--stats" "--width" "240")))
  '(auto-save-default nil)
  '(coffee-tab-width 2)
  '(column-number-mode t)
