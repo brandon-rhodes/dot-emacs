1ac284221a420e7df77d02d4027b69ba807a402b
Brandon Rhodes <brandon@rhodesmill.org>
2018-06-04 15:27:10 -0400

    From old laptop: an “unfill paragraph” command

--- init.el
+++ init.el
@@ -232,6 +232,18 @@
             (make-local-variable 'js-indent-level)
             (setq js-indent-level 2)))
 
+;;; https://www.emacswiki.org/emacs/UnfillParagraph
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
 ;; Ispell should stop suggesting that it can cure a misspelling by
 ;; splitting a word into two quite different words.
 
