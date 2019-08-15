77527fe45415225e89613571a11bbf05a9b8306b
Brandon Rhodes <brandon@rhodesmill.org>
2019-08-15 14:47:16 -0400

    Turn off terrible Magit git commit mode
    Somehow, when Magit detected that a git commit message was being
    written, it took control and not only produced an utterly incorrect
    summary buffer claiming to show the changes being committed (depending
    on its mood, it would either show zero changes or show that every file
    was changing), but arranged for a normal C-x C-c exit to cause git to
    error out and throw away the commit message.  Instead, only C-c C-c
    would exit and preserve the painstakingly written message — but I almost
    never remembered to type that sequence.
    
    This setting lets me take control back and have a normal non-broken
    experience when writing a commit message.  Since I have git display a
    diff directly in the commit message buffer, I already have the main
    functionality that Magit was trying to provide anyway.

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
  '(coffee-tab-width 2)
  '(column-number-mode t)
  '(fill-column 72)
+ '(global-git-commit-mode nil)
  '(global-hl-line-mode t)
  '(ido-enable-flex-matching t)
  '(ido-enable-last-directory-history nil)
