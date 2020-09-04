1a184c2807093279501c0d100c48c03c7ddc3f1b
Brandon Rhodes <brandon@rhodesmill.org>
2020-09-04 03:48:53 -0400

    Add renumber-list command

--- init.el
+++ init.el
@@ -754,3 +754,18 @@ insert straight double quotes instead."
 ;; predictable, and resembles "fzf" which is one of my favorite tools.
 
 (ivy-mode 1)
+
+;; With much thanks to: https://www.emacswiki.org/emacs/RenumberList
+
+(defun renumber-list (start end &optional num)
+      "Renumber the list items in the current START..END region.
+    If optional prefix arg NUM is given, start numbering from that number
+    instead of 1."
+      (interactive "*r\np")
+      (save-excursion
+        (goto-char start)
+        (setq num (or num 1))
+        (save-match-data
+          (while (re-search-forward "^[0-9]+" end t)
+            (replace-match (number-to-string num))
+            (setq num (1+ num))))))
