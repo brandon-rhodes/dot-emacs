b18b46a6d1b0a1de195fbece3ec7f76fec06ae5f
Brandon Rhodes <brandon@rhodesmill.org>
2024-03-30 21:23:19 -0400

    Fix error about "consult-xref"
    The error that was displayed as I started typing in Emacs was:
    ```
    Error (use-package): consult/:config: consult-xref is neither a Command command nor a source
    ```

--- init.el
+++ init.el
@@ -1037,7 +1037,7 @@ insert straight double quotes instead."
   (consult-customize
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
+   consult-bookmark consult-recent-file ;;consult-xref
    consult--source-bookmark consult--source-file-register
    consult--source-recent-file consult--source-project-recent-file
    ;; :preview-key "M-."
