913ea75c0f7b1fc38c013058777991b51d69ac4d
Brandon Rhodes <brandon@rhodesmill.org>
2025-05-13 09:29:23 -0400

    Try turning off previews for ‘consult-buffer’
    This is one of those cases where I consciously find something annoying,
    but might be sometimes relying on the behavior without knowing it, so
    turning it off is a bit of an experiment, to see which.

--- init.el
+++ init.el
@@ -672,6 +672,7 @@ insert straight double quotes instead."
   ;; For some commands and buffer sources it is useful to configure the
   ;; :preview-key on a per-command basis using the `consult-customize' macro.
   (consult-customize
+   consult-buffer :preview-key nil  ;; disable, since I find it disorienting
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file ;;consult-xref
