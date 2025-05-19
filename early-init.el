3d142b12b1d1fc8b83076e027f493f34b37c51a5
Brandon Rhodes <brandon@rhodesmill.org>
2025-05-19 09:46:09 -0400

    Detect Termux and dodge two early-init errors

--- early-init.el
+++ early-init.el
@@ -1,6 +1,9 @@
 ;; Per the straight.el README:
 (setq package-enable-at-startup nil)
 
+;; Flag that indicates whether we are running under Termux.
+(setq this-is-termux (string-match-p "android" operating-system-release))
+
 ;; Per Bozhidar Batsov blog post 2025-03-28: temporarily pause GC during
 ;; startup, then GC every 50MB; and avoid flicker of GUI and theme.
 
@@ -9,6 +12,7 @@
           (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
 
 (menu-bar-mode -1)
-(tool-bar-mode -1)
 (setq inhibit-startup-screen t)
-(load-theme 'dichromacy t)
+(unless this-is-termux
+    (tool-bar-mode -1)
+    (load-theme 'dichromacy t))
